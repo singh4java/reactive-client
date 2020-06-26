0378e85bc56fdde87ffdfdaf2fc7d1297ffec195
manvendrasingh <manvendra80386@gmail.com>
2020-06-26 16:36:50 +0530

    reactive client

---
+++ src/main/java/com/reactive/webclient/reactiveclient/constants/ItemConstants.java
@@ -0,0 +1,8 @@
+package com.reactive.webclient.reactiveclient.constants;
+
+public class ItemConstants {
+
+  public static final String ITEM_END_POINT_V1 = "/v1/items";
+  public static final String ITEM_END_POINT_V1_FUNCTIONAL = "/v1/function/items";
+
+}
--- src/main/java/com/reactive/webclient/reactiveclient/controller/ItemClientController.java
+++ src/main/java/com/reactive/webclient/reactiveclient/controller/ItemClientController.java
@@ -1,8 +1,33 @@
 package com.reactive.webclient.reactiveclient.controller;
 
+import com.reactive.webclient.reactiveclient.constants.ItemConstants;
+import com.reactive.webclient.reactiveclient.domain.Item;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ItemClientController {
+
+  WebClient webClient = WebClient.create("http://localhost:8080");
+
+  @GetMapping("/client/retrieve")
+  public Flux<Item> getAllItemsUsingRetrieve(){
+    return webClient.get()
+        .uri(ItemConstants.ITEM_END_POINT_V1)
+        .retrieve()
+        .bodyToFlux(Item.class)
+        .log("Items in client retrieve : ");
+  }
+
+  @GetMapping("/client/exchange")
+  public Flux<Item> getAllItemsUsingExchange(){
+    return webClient.get()
+        .uri(ItemConstants.ITEM_END_POINT_V1)
+        .exchange()
+        .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Item.class))
+        .log("Items in client exchange: ");
+  }
 
 }
